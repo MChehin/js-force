5ad13274b0bf925f247fe21a92de03e3e10a9721
maria <mariachehin1@gmail.com>
2022-03-24 15:12:23 -0300

    archivo JSON

---
+++ js/json.js
@@ -0,0 +1,26 @@
+const saveDatos = ()=> {
+    //debugger
+    const datosForm = {inputNombre:"", inputTelefono:0, inputEmail:""}
+    datosForm.inputNombre = inputNombre.value 
+    datosForm.inputTelefono = inputTelefono.value 
+    datosForm.inputEmail = inputEmail.value
+    localStorage.setItem("datosDelForm", JSON.stringify(datosForm))  
+    console.info("Se almacenó el array en LocalStorage")
+}
+
+btnSubmit.addEventListener("click", saveDatos)
+
+const obtenerDatos = ()=> {
+    //debugger
+    if (localStorage.getItem("datosDelForm") !=null) {
+        datosDelForm =JSON.parse(localStorage.getItem("datosDelForm"))
+        inputNombre.value = datosDelForm.inputNombre
+        inputTelefono.value = datosDelForm.inputTelefono
+        inputEmail.value = datosDelForm.inputEmail
+    }
+    datosDelForm = JSON.parse("datosDelForm")
+
+}
+
+obtenerDatos()
+
